02e7b8faaf5538e0f526c43a8e7965610b7d97eb
Evan Levine <elevine30@gmail.com>
2013-07-11 15:22:30 -0400

    Work on constructor in the database class

--- database.py
+++ database.py
@@ -1,3 +1,18 @@
+import cPickle as pickle
+
+class Database(object):
+   
+   def __init__(self,filename):
+       #open and read the file
+       with open(filename,'r') as f:
+           pickle.load(f)
+       
+   def getColumn(name):
+       return self.field(name)
+   
+
+
+
 def simple(x):
 	return x
 

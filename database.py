09127301bce418c9c55875351efa0408870f03e2
Brian <cherinka@di.utoronto.ca>
2013-07-11 16:51:20 -0400

    modified database.py to read in FITS files

--- database.py
+++ database.py
@@ -5,9 +5,9 @@ class Database(object):
    
    def __init__(self,filename):
        #open and read the file
-       self.hdulist=fits.open(filename)
-       self.header = hdulist[1].header
-       self.data = hdulist[1].data
+       self.hdulist = fits.open(filename)
+       self.header = self.hdulist[1].header
+       self.data = self.hdulist[1].data
        
    def getColumn(self,name):
        return self.data.field(name)

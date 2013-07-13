e8928e65a5beebea8ee2cf1e2e106ada11816f4b
Evan Levine <elevine30@gmail.com>
2013-07-13 10:56:33 -0400

    Uploading the few changes I made to astroobjects

--- astroobjects.py
+++ astroobjects.py
@@ -1,6 +1,19 @@
-
-
-def simple2(x):
-	return x
-
-
+import math
+class Astroobjects(object):
+    def __init__(self,b,l,hdist):
+        '''
+        designed to store b and l in degrees, not radians
+        hdist in kpc
+        '''
+        self.b=b
+        self.l=l
+        self.hdist=hdist
+        
+    def galXYZ(self):
+        '''
+        Returns xyz in kpc
+        '''
+        x=self.hdist*math.sin(self.b*math.pi/180.)*math.cos(self.l*math.pi/180.)-8.
+        y=self.hdist*math.sin(self.b*math.pi/180.)*math.sin(self.l*math.pi/180.)
+        z=self.hdist*math.cos(self.b*math.pi/180.)
+        return [x,y,z]

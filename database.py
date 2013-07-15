450399129d846ffd9218c0d3df38ea6a6f574fce
Brian <cherinka@di.utoronto.ca>
2013-07-15 10:55:08 -0400

    uploading my changes to database.py

--- database.py
+++ database.py
@@ -1,5 +1,10 @@
-import cPickle as pickle
 from astropy.io import fits
+import numpy as np
+import pdb
+
+#pdb.pm() - inserts me back into problem area
+#pdb.set_trace() - put stop point
+#n (next line), s (steps through functions), q (quit), u (up), c (continue)
 
 class Database(object):
    
@@ -12,5 +17,39 @@ class Database(object):
    def getColumn(self,name):
        return self.data.field(name)
    
+   def removeBad(self, column, tag):
+		
+		#cast into numpy arrays
+		tag = np.array(tag)
+		column = np.array(column)
+				
+		print tag
+		print column
+		
+		#check sizes of each and return error if tag != 1 or != to column
+		ntag = tag.size
+		ncol = column.size
+		
+		print 'ntag:', ntag
+		print 'ncol:', ncol
+		
+		pdb.set_trace()
+		if (ntag > 1 and ntag != ncol):
+			print 'Error: tag must be either 1 or same size as column'
+			return
 
+		if ntag == 1:
+			tag=np.resize(tag,ncol)	
 
+		cond = self.data[str(column[0])] != tag[0]
+		if ncol > 1:
+			for i in range(ncol)[1:]:
+				cond = cond & (self.data[str(column[i])] != tag[i])
+
+		#select out good data
+   		good, = np.where(cond)
+
+   		return self.data[good]
+   	
+
+		
